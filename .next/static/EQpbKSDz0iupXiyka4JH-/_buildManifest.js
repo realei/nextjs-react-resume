c4bd548118523b08e28ac504044f93d95c88c5d9
Lei Wang <wanglei.okay@gmail.com>
2024-03-13 21:10:41 +0200

    update github pages

--- .next/static/EQpbKSDz0iupXiyka4JH-/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/250-5136cdec8a9c0673.js","static/chunks/pages/index-414d226da5889ffe.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
